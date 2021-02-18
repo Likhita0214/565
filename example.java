78659bce4648ca00c16da2afde66360433ada3f7
Likhita0214 <79193630+Likhita0214@users.noreply.github.com>
2021-02-18 11:27:23 +0530

    Update example.java

--- example.java
+++ example.java
@@ -6,6 +6,7 @@ public class example
          System.out.println("hello hi you are entering github");
          System.out.println("Heyy!!! i rewritten your code");
 	 System.out.println("I am 579 supraja");
+	 System.out.println("thankyou!!!");
    }
 }
 
